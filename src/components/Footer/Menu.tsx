b41a0110289f9b06a5db6454503109521e57ecda
Tannaz Shirzadi <tia.shr97@gmail.com>
2024-02-08 16:20:37 +0300

    feat: desktop view

--- src/components/Footer/Menu.tsx
+++ src/components/Footer/Menu.tsx
@@ -10,11 +10,15 @@ const FooterMenu = () => {
       className="mt-12 text-sm font-bold tracking-wide uppercase flex flex-col items-center
      md:flex-row"
     >
-      <Link href={"/"} className="mb-4">
+      <Link href={"/"} className="mb-4 hover:text-orange-dark">
         home
       </Link>
       {MenuItems.map((item: IHeaderMenuItem) => (
-        <Link key={item.name} href={item.href} className="mb-4 md:ml-8">
+        <Link
+          key={item.name}
+          href={item.href}
+          className="mb-4 md:ml-8 hover:text-orange-dark"
+        >
           {item.name}
         </Link>
       ))}
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -11,34 +11,36 @@ import FooterMenu from "./Menu";
 const Footer = () => {
   return (
     <div
-      className=" bg-black flex mt-auto text-white flex-col items-center 
-    md:items-start md:pl-10"
+      className="bg-black flex mt-auto text-white flex-col items-center 
+    md:items-start md:pl-10 lg:px-40"
     >
       <div className="w-24 border-t-4 border-orange-dark" />
-      <LogoIcon className="mt-12" />
-      <FooterMenu />
-      <P className=" text-border-grey text-center md:text-start mt-10 mx-6 md:ml-0">
+      <div className="lg:flex w-full lg:justify-between">
+        <LogoIcon className="mt-12 mx-auto md:ml-0" />
+        <FooterMenu />
+      </div>
+      <P className=" text-border-grey text-center md:text-start mt-10 mx-6 md:ml-0 lg:w-[540px]">
         Audiophile is an all in one stop to fulfill your audio needs. We&apos;re
         a small team of music lovers and sound specialists who are devoted to
         helping you get the most out of personal audio. Come and visit our demo
         facility - we&apos;re open 7 days a week.
       </P>
       <div
-        className=" mb-4 flex items-center flex-col mx-6 mt-10 
+        className="lg:relative mb-4 flex items-center flex-col mx-6 mt-10 
       md:flex-row md:justify-between md:w-full md:pr-6 md:ml-0"
       >
         <P className=" text-border-grey text-center">
           Copyright 2021. All Rights Reserved
         </P>
-        <div className="flex gap-4 mt-10 md:mt-0">
+        <div className="flex gap-4 mt-10 md:mt-0 lg:absolute lg:right-0 lg:top-[-50px]">
           <Link href="#">
-            <IconFacebook />
+            <IconFacebook className=" fill-white hover:fill-orange-dark" />
           </Link>
           <Link href="#">
-            <IconInstagram />
+            <IconInstagram className=" fill-white hover:fill-orange-dark" />
           </Link>
           <Link href="#">
-            <IconTwitter />
+            <IconTwitter className=" fill-white hover:fill-orange-dark" />
           </Link>
         </div>
       </div>
